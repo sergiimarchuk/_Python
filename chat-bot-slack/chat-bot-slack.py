8fe5aef712a9f1295e7bd262cf94fb904f8ebe46
sergiimarchuk <sergii.marchuk@gmail.com>
2022-12-09 18:10:00 +0200

    Update chat-bot-slack.py

--- chat-bot-slack/chat-bot-slack.py
+++ chat-bot-slack/chat-bot-slack.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 import os
 import subprocess
 import ast
@@ -11,93 +10,67 @@ import socket
 import smtplib
 from email.mime.text import MIMEText
 from datetime import datetime
+from configparser import ConfigParser
+
 
 #variables
 hostname = socket.gethostname()
 datetime_var = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-template_message = "Customer ... environment is .."
+template_message = "Customer Cashtie. "
 path_to_aws_util = ''
 message_alert_notify = template_message + ' Please check instance   ' + hostname +  '   seems log agent has been stopped. ' + datetime_var
 body_alert_notify = 'Hello, seems log agent stopped or not running on server ' + hostname
 body_ok_notify = 'Hello, amazon-cloudwatch-agent is running. Status OK.'
-subject_alert = ' Please check' 
-subject_ok = ' Status OK'
+subject_alert = ' Please check amazon-cloudwatch-agent' 
+subject_ok = ' Status amazon-cloudwatch-agent OK'
 message_slack_alert = template_message + ' Please check instance   ' + hostname +  '   seems log agent has been stopped.'
-print(datetime_var)
+message_slack_ok = template_message + ' amazon-cloudwatch-agent is up and running now on host ' + hostname
 file_log_name = 'status-log-agent.log'
-
-
-# find util agent cloudwatch needs to implement, still not finish this function 
-def existsUtil():
-        check_command = "/usr/bin/whereis amazon-cloudwatch-agent-ctl"
-        run_check_command = subprocess.check_output(['bash','-c', check_command])
-        out_run_command = (((str(run_check_command)).split(':')[1]).replace('\\n\'','')).replace(' ','')
-        try:
-            if (len(out_run_command)) > 5:
-                path_to_aws_util = out_run_command
-            else:
-                print("Warning aws util is not installed")
-        except:
-            print("An exception occurred")
-        return(out_run_command)
-print(existsUtil())
 
 # check status amazon cloud watch agent 
 def getStatusLogAgent():
-        check_command = "/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a  status"
+        check_command = "/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status"
         run_check_command = subprocess.check_output(['bash','-c', check_command])
         dict_status = ast.literal_eval(run_check_command.decode("UTF-8"))
         #status = dict_status.get('status')
         status = dict_status['status']
         return(status)
 
-# check is it running or not agent amazon needs update and double check
-def checkProcessLogAgent():
-        check_command = "ps -ef | grep -i agent | grep -v grep | wc -l"
-        run_check_command = (subprocess.check_output(['bash','-c', check_command])).decode("UTF-8")
-        #count_process = run_check_command.decode("UTF-8")
-        count_process = os.linesep.join([s for s in run_check_command.splitlines() if s])
-        return(count_process)
-
 # function send message into slack channel via slack-bot about alert 
 def send_slack_message(message):
     payload = '{"text":"%s"}' % message
-    response = requests.post('https://hooks.slack.com/services/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', data=payload)
-    print(response.text)
+    response = requests.post('https://hooks.slack.com/services/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', data=payload)
+#    print(response.text)
 
 # notify function massage into slack channel 
 def notifyAlertSlack(arg1):
-    #message = template_message + ' Please check instance   ' + hostname +  '   seems log agent has been stopped.'
     send_slack_message(arg1)
 
 #send alert message via email, account has to be create as technical to space gmail io
 def sendMessageEmail(arg1,arg2):
+    config = ConfigParser()
+    config.read('/etc/secrets/config.ini')
+    email = config.get('notify', 'email_sender')
+    password = config.get('notify', 'password')
+
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
-    server.login('sergii.marchuk@eteam.io','pass')
+    server.login(email,password)
     msg = MIMEText(arg1)
-    msg['Subject'] = 'Env. CAS. ' + hostname + arg2
-    
-    #server.sendmail('from', 'to', 'message')
-    server.sendmail('sergii.marchuk@eteam.io', 'sergii.marchuk@eteam.io', msg.as_string())
-    #print('Mail has been sent')
+    msg['Subject'] = template_message + ' ' + hostname + ' ' + arg2
 
-def readLogStatus():
-    with open(file_log_name, "rb") as fp:
-        last_line = fp.readlines()[-1]
-        print(last_line)
-#readLogStatus()
+    email_receiver = config.get('notify', 'email_receiver')
+    #server.sendmail('from', 'to', 'message')
+    server.sendmail(email, email_receiver, msg.as_string())
 
 def logStatus(arg1,arg2):
 #    with open(file_log_name, 'w'): pass
     with open (file_log_name, 'a') as f: f.write (datetime_var + ' ' + hostname + ' ' + arg1 + ' ' + arg2  + '\n')
 
 def main():
-    if int(checkProcessLogAgent()) > 0:
         if (getStatusLogAgent()) == 'stopped':
             if os.path.isfile(file_log_name) == False:
                 notifyAlertSlack(message_slack_alert)
-                #sendMessageEmail()
                 sendMessageEmail(message_alert_notify, subject_alert)
                 logStatus('status-cloudwatch-agent: agent stopped or not running','notify: has-been-sent')
             elif os.path.isfile(file_log_name) == True:
@@ -106,6 +79,7 @@ def main():
             if os.path.isfile(file_log_name) == True:
                 #status-cloudwatch-agent is running we do not do anything, just remove file status-log 
                 sendMessageEmail(body_ok_notify, subject_ok)
+                notifyAlertSlack(message_slack_ok)
                 if os.path.isfile(file_log_name):
                     os.remove(file_log_name)
                 else:    ## Show an error ##
